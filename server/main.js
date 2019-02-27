b63b21554d11fe56546af8924cf1f17a94e579a4
schippas <schippas@purdue.edu>
2019-02-27 13:31:51 -0500

    Setup MySQL Server and Database connection

--- server/main.js
+++ server/main.js
@@ -16,3 +16,21 @@ app.get('/', function (req, res) {
 })
 
 app.listen(port, () => console.log(`Anthologist backend server listening on port ${port}!`))
+
+//MySQL Stuff here
+let mysql = require('mysql');
+let connection = mysql.createConnection({
+    host: 'thestrugglingengineer.com',
+    port: '3306',
+    user: 'samuel',
+    password: 'Anthologist',
+    database: 'Anthologist'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+ 
+  console.log('Connected to the MySQL server.');
+});
