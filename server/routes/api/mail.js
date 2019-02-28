8aab3fd022ac5fbdec95be2f7f54412cdad815c6
Sam <sam.e.deutsch@gmail.com>
2019-02-28 06:30:06 -0500

    Added the reset password call

--- server/routes/api/mail.js
+++ server/routes/api/mail.js
@@ -96,7 +96,9 @@ module.exports = (app) => {
         const token = req.body.token
         const verkey = req.body.verify
 
-        if ( !isuuid.v4(account) )
+        if ( !isuuid.v4(account)
+        || token.length != 64
+        || verkey.length != 64 )
         {
             console.log( "verify: invalid account id" );
             return res.sendStatus(400)
@@ -156,7 +158,7 @@ module.exports = (app) => {
         if(!req.body
         || !req.body.email )
         {
-            console.log( 'login: missing field' )
+            console.log( 'send-reset: missing field' )
             return res.sendStatus(400)
         }
 
@@ -164,7 +166,7 @@ module.exports = (app) => {
 
         if( email.length > 254 )
         {
-            console.log( 'login: field too long' )
+            console.log( 'send-reset: field too long' )
             return res.sendStatus(400)
         }
 
@@ -172,12 +174,12 @@ module.exports = (app) => {
         {
             if ( err )
             {
-                console.log( "reset: sql_error: ", err );
+                console.log( "send-reset: sql_error: ", err );
                 res.json({"status":"fail","reason":"un-caught sql error"});
             }
             else if ( rsql.length == 0 )
             {
-                console.log( "reset: no account with email: " + email );
+                console.log( "send-reset: no account with email: " + email );
                 res.json({"status":"fail","reason":"no account with that email"});
             }
             else
@@ -197,8 +199,67 @@ module.exports = (app) => {
 
                 sqlsec.query("UPDATE accounts SET reset=? WHERE id=?;", [ reskey, rsql[0].id ] );
 
-                console.log( "reset: password reset email sent to: " + email )
+                console.log( "send-reset: password reset email sent to: " + email )
                 res.json({"status":"okay"})
+            }
+        });
+    });
+
+    app.post('/api/reset', function(req, res) {
+
+        if(!req.body
+        || !req.body.account
+        || !req.body.reset
+        || !req.body.password )
+        {
+            console.log( 'reset: missing field' )
+            return res.sendStatus(400)
+        }
+
+        const account = req.body.account
+        const reskey = req.body.reset
+        const password = req.body.password
+
+        if ( !isuuid.v4(account)
+        || reskey.length != 64 )
+        {
+            console.log( 'reset: invalid field' )
+            return res.sendStatus(400)
+        }
+
+        const salt = crypto.randomBytes(32);
+        const hashpass = crypto.pbkdf2Sync( password, salt, 100000, 32, "sha512" );
+        const token = crypto.randomBytes(32).toString('hex');
+
+        sqlcon.query( "SELECT reset FROM accounts WHERE id=?;", [ account ], function( err, rsql )
+        {
+            if ( err )
+            {
+                console.log( "reset: sql_error: ", err )
+                res.json({"status":"fail","reason":"un-caught sql error"})
+            }
+            else if ( rsql.length == 0 )
+            {
+                console.log( "reset: no account with id: " + account )
+                res.json({"status":"fail","reason":"no account with that id"})
+            }
+            else if ( rsql[0].reset === "no_reset" )
+            {
+                console.log( "reset: no reset requests pending for account: " + account )
+                res.json({"status":"fail","reason":"no resets pending"})
+            }
+            else if ( rsql[0].reset !== reskey )
+            {
+                console.log( "reset: incorrect reset key for account: " + account )
+                res.json({"status":"fail","reason":"incorrect reset key"})
+            }
+            else
+            {
+                sqlsec.query( "UPDATE accounts SET password=?, salt=?, token=?, reset='no_reset' WHERE id=?;",
+                    [hashpass.toString('hex'), salt.toString('hex'), token, account] );
+
+                console.log( "reset: password reset for account: " + account )
+                res.json({"status":"okay","account":account,"token":token})
             }
         });
     });
