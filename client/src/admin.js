fcfded92b2669ff431fc8065523990e93d99758b
Hadeed Noshab <hnoshab@Hadeeds-MacBook-Pro.local>
2019-03-22 14:59:04 -0400

    Admin page updated

--- client/src/admin.js
+++ client/src/admin.js
@@ -4,6 +4,13 @@ import './admin.css';
 
 class admin extends Component {
 
+    onDeleteAccount(){
+        document.getElementById('admin_id_input').value = "";
+    }
+
+    onDeleteStory(){
+        document.getElementById('admin_id_input').value = "";
+    }
       
       render() {
         return (
@@ -34,19 +41,19 @@ class admin extends Component {
           <div id = "story-title" className="StoryTitle"> Admin Control<a className="title" href> </a> </div>
   
           {/* Entry points */}
-          <div className='inputs_new'>
+          <div className='a_inputs_new'>
               
               <div><span className='st'>User ID : </span>
-                  <input className='input_new' type="text" id="book_title"/>
+                  <input className='admin_input' type="text" id="admin_id_input"/>
               </div>
-              <div className = 'button_pos'  >
-                  <button className="add-button" 
-                  id= "add_story" onClick={() => this.onDeleteAccount()}>Add</button>
+              <div className = 'a_button_pos'  >
+                  <button className="admin-button" 
+                  id= "add_story" onClick={() => this.onDeleteAccount()}> Delete Account</button>
                   </div>
 
-             <div className = 'button_pos'  >
-                  <button className="add-button" 
-                  id= "add_story" onClick={() => this.onDeleteStory()}>Add</button>
+             <div className = 'a_button_pos'  >
+                  <button className="admin-button" 
+                  id= "add_story" onClick={() => this.onDeleteStory()}>Delete Story</button>
                   </div>
          
   
