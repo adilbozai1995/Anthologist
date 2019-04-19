6402f66835e4ca67b07889453f8b1bd5bfe65b19
Hadeed Noshab <hnoshab@pal-nat186-55-51.itap.purdue.edu>
2019-04-19 16:58:51 -0400

    Modal styles

--- client/src/story.js
+++ client/src/story.js
@@ -202,12 +202,6 @@ class story extends Component {
                             document.getElementById('nCount').innerHTML = seconds + " Until voting ends";
                         }
 
-                        if ( response.votestart + (response.votetime * 60 ) + 10 < Date.now() / 1000 )
-                        {
-                            // Story should have update by now
-                            window.location.reload()
-                        }
-
                     }, 1000 );
                 }
                 else
@@ -528,11 +522,31 @@ onClickLike = (blockId) => {
         }
 
       
-      <Modal  /*THIS A STORY YOU NEED TO CHANGE*/
-          isOpen={this.state.StorymodalIsOpen} onRequestClose={this.StorycloseModal}
-          style={{width:'100px'}}
-          
-          >
+      <Modal style={{
+    overlay: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(255, 255, 255, 0.75)'
+    },
+    content: {
+      position: 'absolute',
+      top: '40px',
+      left: '40px',
+      right: '40px',
+      bottom: '40px',
+      border: '1px solid #ccc',
+      background: '#fff',
+      overflow: 'auto',
+      WebkitOverflowScrolling: 'touch',
+      borderRadius: '4px',
+      outline: 'none',
+      padding: '20px'
+    }
+  }}/*THIS A STORY YOU NEED TO CHANGE*/
+          isOpen={this.state.StorymodalIsOpen} onRequestClose={this.StorycloseModal}>
           <button onClick={this.StorycloseModal}>close</button>
           <div>{this.state.StoryModalText}</div>
         </Modal>
